9d6f6a129f47571fca1078760b465d5801bc74ae
KevinMarete <kevomarete@gmail.com>
2021-08-03 21:03:00 +0300

    added menu group migration

---
+++ database/migrations/2021_07_26_173005_create_menu_group_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_menu_group', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('icon');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['name']);
+        });
+
+        //Add default data
+        DB::table('tbl_menu_group')->insert(
+            array(
+                ['name' => 'Dashboard', 'icon' => 'fas fa-tachometer-alt', 'created_at' => now()],
+                ['name' => 'Data', 'icon' => 'fas fa-database', 'created_at' => now()],
+                ['name' => 'Settings', 'icon' => 'fas fa-cog', 'created_at' => now()]
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_menu_group');
+    }
+}
