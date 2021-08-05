820397c17e832179ed59720fc9527c0c0cb5ff9c
KevinMarete <kevomarete@gmail.com>
2021-08-05 03:02:13 +0300

    added chart config to dashboard controller

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -10,7 +10,33 @@ class DashboardController extends BaseController
 {
   public function displayDashboardView(Request $request)
   {
-    $view_data = [];
+    $view_data = [
+      'chart_data' => json_encode([
+        'title' => 'Current on ART vs Current on Care',
+        'type' => 'horizontalBar',
+        'labels' => [
+          "20+ yrs (Adults)",
+          "10-19 yrs (Teens)",
+          "0-9 yrs (Children)",
+          "Total",
+        ],
+        'datasets' => [
+          [
+            "label" => "Current On ART",
+            "data" => [200, 40, 6, 246],
+            "borderColor" => "transparent",
+            "backgroundColor" => "#FFFF00",
+          ],
+          [
+            "label" => "Current On Care",
+            "data" => [-300, -55, -15, -370],
+            "borderColor" => "transparent",
+            "backgroundColor" => "#0000FF",
+          ],
+        ]
+      ])
+    ];
+
     $category = $request->category;
 
     $token = session()->get('token');
@@ -21,7 +47,7 @@ class DashboardController extends BaseController
       'main_menu' => 'dashboard',
       'sub_menu' => $category,
       'menugroups' => $this->getRoleMenus($token, $role_id),
-      'content_view' => View::make('dashboard.'.$category, $view_data),
+      'content_view' => View::make('dashboard.' . $category, $view_data),
     ];
 
     return view('template.main', $data);
