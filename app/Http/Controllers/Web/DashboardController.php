517db4c7f9d123b5a7ba30fdd573e901c5101486
KevinMarete <kevomarete@gmail.com>
2021-08-06 04:26:16 +0300

    updated dashboard web controller

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -6,41 +6,42 @@ use App\Http\Controllers\Web\BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
+use function GuzzleHttp\json_decode;
+
 class DashboardController extends BaseController
 {
   public function displayDashboardView(Request $request)
   {
-    $view_data = [
-      'chart_data' => json_encode([
-        'title' => 'Current on ART vs Current on Care',
-        'type' => 'horizontalBar',
-        'labels' => [
-          "20+ yrs (Adults)",
-          "10-19 yrs (Teens)",
-          "0-9 yrs (Children)",
-          "Total",
-        ],
-        'datasets' => [
-          [
-            "label" => "Current On ART",
-            "data" => [200, 40, 6, 246],
-            "borderColor" => "transparent",
-            "backgroundColor" => "#FFFF00",
-          ],
-          [
-            "label" => "Current On Care",
-            "data" => [-300, -55, -15, -370],
-            "borderColor" => "transparent",
-            "backgroundColor" => "#0000FF",
-          ],
-        ]
-      ])
-    ];
-
+    $from = '2020-02-01';
+    $to = '2020-03-01';
     $category = $request->category;
 
     $token = session()->get('token');
     $role_id = session()->get('role_id');
+
+    $config = config('dashboard');
+    $chart_config = $config[$request->category]['charts'];
+
+    foreach ($chart_config as $chart_name => $chart) {
+      $datasets = $chart['datasets'];
+
+      foreach ($datasets as $index => $dataset) {
+        $data_url = sprintf($dataset['dataUrl'], $from, $to);
+        $chart_data = $this->getChartData($token, $data_url);
+
+        $chart['labels'] = $chart_data['labels'];
+        $chart['datasets'][$index]['data'] = $chart_data['data'];
+      }
+      $chart_config[$chart_name] = json_encode($chart);
+    }
+
+    $view_data = [
+      'chart_config' => $chart_config,
+      'counties' => [],
+      'facilities' => [],
+      'from' => $from,
+      'to' => $to
+    ];
 
     $data = [
       'page_title' => 'Home',
@@ -51,5 +52,21 @@ class DashboardController extends BaseController
     ];
 
     return view('template.main', $data);
+  }
+
+  public function displayDashboardFilterView(Request $request)
+  {
+    return redirect('/dashboard/care-and-treatment');
+  }
+
+  public function getChartData($token = null, $data_url = null)
+  {
+    $response = ['labels' => [], 'data' => []];
+    if ($token !== null && $data_url !== null) {
+      $results = $this->manageResourceData($token, 'GET', $data_url);
+      $response['labels'] = array_keys($results);
+      $response['data'] = array_values($results);
+    }
+    return $response;
   }
 }
