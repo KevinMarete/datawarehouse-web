32ae7e1e5524e9a0ecbd3955d12d9eac94cd916b
KevinMarete <kevomarete@gmail.com>
2021-08-06 04:26:03 +0300

    added data api functionality

--- app/Http/Controllers/Api/DataController.php
+++ app/Http/Controllers/Api/DataController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
 class DataController extends Controller
 {
 
-  protected $default_results;
+  protected $default_results, $default_age_gender;
 
   public function __construct()
   {
@@ -17,6 +17,33 @@ class DataController extends Controller
       '10 - 19 yrs (Adolescents)' => 0,
       '0 - 9 yrs (Children)' => 0,
       'Total' => 0
+    ];
+
+    $this->default_age_gender_results = [
+      '<1 F' => 0,
+      '<1 M' => 0,
+      '1-4 F' => 0,
+      '1-4 M' => 0,
+      '5-9 F' => 0,
+      '5-9 M' => 0,
+      '10-14 F' => 0,
+      '10-14 M' => 0,
+      '15-19 F' => 0,
+      '15-19 M' => 0,
+      '20-24 F' => 0,
+      '20-24 M' => 0,
+      '25-29 F' => 0,
+      '25-29 M' => 0,
+      '30-34 F' => 0,
+      '30-34 M' => 0,
+      '35-39 F' => 0,
+      '35-39 M' => 0,
+      '40-44 F' => 0,
+      '40-44 M' => 0,
+      '45-49 F' => 0,
+      '45-49 M' => 0,
+      '50+ F' => 0,
+      '50+ M' => 0,
     ];
   }
 
@@ -30,7 +57,7 @@ class DataController extends Controller
    */
   public function getOnART($category, $from, $to)
   {
-    if ($category == 'new') {
+    if ($category == 'new' || $category == 'new-age-gender') {
       $results = Patient::whereDate('start_regimen_date', '>=', $from)
         ->whereDate('start_regimen_date', '<', $to)
         ->get();
@@ -38,13 +65,36 @@ class DataController extends Controller
       $results = Patient::whereDate('start_regimen_date', '<', $to)->get();
     }
 
-    $results = $this->arrayGroupBy($results, 'age_group');
-    $results = $this->arrayCount($results);
+    if ($category == 'new-age-gender' || $category == 'current-age-gender') {
+      $results = $this->arrayGroupBy($results, 'age_group_gender');
+      $results = $this->arrayCount($results);
 
-    $results['Total'] = array_sum(array_values($results));
+      $results = array_merge($this->default_age_gender_results, $results);
+    } else if ($category == 'current-prop') {
+      $enrollment_results = Patient::whereDate('enrollment_date', '<', $to)->get();
 
-    $results = array_merge($this->default_results, $results);
+      $art_results = $this->arrayGroupBy($results, 'age_group_gender');
+      $art_results = $this->arrayCount($art_results);
+      $art_results = array_merge($this->default_age_gender_results, $art_results);
 
+      $enrollment_results = $this->arrayGroupBy($enrollment_results, 'age_group_gender');
+      $enrollment_results = $this->arrayCount($enrollment_results);
+      $enrollment_results = array_merge($this->default_age_gender_results, $enrollment_results);
+
+      $results = [];
+      foreach ($enrollment_results as $key => $result) {
+        $results[$key] = 0;
+        if ($result > 0) {
+          $results[$key] = round(($art_results[$key] / $result) * 100);
+        }
+      }
+    } else {
+      $results = $this->arrayGroupBy($results, 'age_group');
+      $results = $this->arrayCount($results);
+
+      $results['Total'] = array_sum(array_values($results));
+      $results = array_merge($this->default_results, $results);
+    }
     return response()->json($results);
   }
 
@@ -58,7 +108,7 @@ class DataController extends Controller
    */
   public function getOnCare($category, $from, $to)
   {
-    if ($category == 'new') {
+    if ($category == 'new' || $category == 'new-age-gender') {
       $results = Patient::whereDate('enrollment_date', '>=', $from)
         ->whereDate('enrollment_date', '<', $to)
         ->get();
@@ -66,12 +116,18 @@ class DataController extends Controller
       $results = Patient::whereDate('enrollment_date', '<', $to)->get();
     }
 
-    $results = $this->arrayGroupBy($results, 'age_group');
-    $results = $this->arrayCount($results);
+    if ($category == 'new-age-gender' || $category == 'current-age-gender') {
+      $results = $this->arrayGroupBy($results, 'age_group_gender');
+      $results = $this->arrayCount($results);
 
-    $results['Total'] = array_sum(array_values($results));
+      $results = array_merge($this->default_age_gender_results, $results);
+    } else {
+      $results = $this->arrayGroupBy($results, 'age_group');
+      $results = $this->arrayCount($results);
 
-    $results = array_merge($this->default_results, $results);
+      $results['Total'] = array_sum(array_values($results));
+      $results = array_merge($this->default_results, $results);
+    }
 
     return response()->json($results);
   }
