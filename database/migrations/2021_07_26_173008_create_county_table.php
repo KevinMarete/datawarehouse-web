538448e8fa1cf3d12124a4dea1906fd2a5054c1e
KevinMarete <kevomarete@gmail.com>
2021-08-07 13:38:34 +0300

    added migration for county

---
+++ database/migrations/2021_07_26_173008_create_county_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateCountyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_county', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['name']);
+        });
+
+        //Add default data
+        DB::table('tbl_county')->insert(
+            [
+                ['name' => '', 'created_at' => now()],
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_county');
+    }
+}
