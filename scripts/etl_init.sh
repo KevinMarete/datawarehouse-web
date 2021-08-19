9092c582fd0242e3478a04e8ed1f8d3e8847e068
KevinMarete <kevomarete@gmail.com>
2021-08-19 16:43:12 +0300

    removed etl_init script

--- scripts/etl_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -f .env ] 
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-  mysql -h $DB_HOST -P $DB_PORT  -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < $ETL_INIT_FILE 2>/dev/null
-fi
