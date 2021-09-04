8cc5545bd5672c565ed92c679eae6ebd850d7532
KevinMarete <kevomarete@gmail.com>
2021-09-04 21:19:08 +0300

    updated pmtct dashboard view

--- resources/views/dashboard/pmtct-eid-hei-pos-hca.blade.php
+++ resources/views/dashboard/pmtct-eid-hei-pos-hca.blade.php
@@ -4,10 +4,40 @@
       @include('dashboard.filter')
       <div class="row">
         <div class="col-12">
-          <h3 class="title-5 m-3">PMTCT EID & PMTCT HEI POS & HCA</h3>
+          <h3 class="title-5 m-3">PMTCT & EID & HEI POS & HCA</h3>
         </div>
       </div>
       <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
+          <div class="chart-wrap">
+            <canvas id="1024PmtctCascadesChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['10_24_pmtct_cascades_chart'] }}"></canvas>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
+          <div class="chart-wrap">
+            <canvas id="1519PmtctCascadesChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['15_19_pmtct_cascades_chart'] }}"></canvas>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
+          <div class="chart-wrap">
+            <canvas id="2024PmtctCascadesChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['20_24_pmtct_cascades_chart'] }}"></canvas>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
+          <div class="chart-wrap">
+            <canvas id="1524YouthsPmtctCascadesChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['15_24_youths_pmtct_cascades_chart'] }}"></canvas>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
+          <div class="chart-wrap">
+            <canvas id="over25PmtctCascadesChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['over_25_pmtct_cascades_chart'] }}"></canvas>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
+          <div class="chart-wrap">
+            <canvas id="totalsPmtctCascadesChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['totals_pmtct_cascades_chart'] }}"></canvas>
+          </div>
+        </div>
         <div class="col-sm-12 col-md-6 col-lg-6 p-3">
           <div class="chart-wrap">
             <canvas id="pmtctEidPmtctBy12Chart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['pmtct_eid_pmtct_by_12_chart'] }}"></canvas>
