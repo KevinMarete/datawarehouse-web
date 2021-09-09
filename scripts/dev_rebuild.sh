5d6e486eddf3c607bdef1e468d71e7a89200bdf3
KevinMarete <kevomarete@gmail.com>
2021-09-09 15:08:45 +0300

    added dev rebuild script

---
+++ scripts/dev_rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+composer install
+php artisan key:generate
+php artisan config:clear
+php artisan migrate:refresh
+php artisan passport:install
+if [ -f .env ] 
+then
+  echo 'Started creating etl tables...' 
+  export $(cat .env | sed 's/#.*//g' | xargs)
+  mysql -h $DB_HOST -P $DB_PORT  -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < $ETL_INIT_FILE 2>/dev/null
+  echo 'Finished creating etl tables.'
+fi
