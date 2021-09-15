c73bd8da526ea57796fcd878aaefc0c2d1094fb4
KevinMarete <kevomarete@gmail.com>
2021-09-15 23:09:40 +0300

    update cervical screening dashboard charts view

--- resources/views/dashboard/hiv-cervical-cancer.blade.php
+++ resources/views/dashboard/hiv-cervical-cancer.blade.php
@@ -8,12 +8,12 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-6 p-3">
+        <div class="col-sm-12 col-md-8 col-lg-8 p-3">
           <div class="chart-wrap">
             <canvas id="cervicalCancerCascadeChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['cervical_cancer_cascade_chart'] }}"></canvas>
           </div>
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-6 p-3">
+        <div class="col-sm-12 col-md-4 col-lg-4 p-3">
           <div class="chart-wrap">
             <canvas id="cervicalCancerScreeningVisitTypeChart" width="900" height="450" class="dashboard-chart" data-api-resource="{{ $chart_config['cervical_cancer_screening_visit_type_chart'] }}"></canvas>
           </div>
