16c50228e3453d38d0718badfcacaf75e5e0e946
KevinMarete <kevomarete@gmail.com>
2021-09-21 19:01:11 +0300

    add overall mmd api controller function

--- app/Http/Controllers/Api/VisitController.php
+++ app/Http/Controllers/Api/VisitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Patient;
 use App\Models\Visit;
 use App\Http\Controllers\Api\BaseController;
 use Illuminate\Http\Request;
@@ -76,6 +77,44 @@ class VisitController extends BaseController
     $results = $this->arrayGroupBy($results, $group_by_index);
     $results = $this->arrayCount($results);
     $results = array_merge($this->default_results[$group_by_index], $results);
+
+    return response()->json($results);
+  }
+
+    /**
+   * Display overall  multi month dispensing totals on art by age group
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function getOverallMultiMonthDispensingTotalsByAgeGroupGender(Request $request)
+  {
+    $from = $request->from;
+    $to = $request->to;
+    $group_by_index = 'age_group_gender';
+    $filters = [
+      'facility' => $request->facility,
+      'sub_county' => $request->subcounty
+    ];
+    $results = [];
+
+    $visit_results = Visit::whereDate('visit_date', '>=', $from)->whereDate('visit_date', '<', $to)->whereRaw('datediff(next_appointment_date, visit_date) > ?', [30])->get()->toArray();
+    $visit_results = $this->arrayFilterBy($visit_results, $filters);
+    $visit_results = $this->arrayGroupBy($visit_results, $group_by_index);
+    $visit_results = $this->arrayCount($visit_results);
+    $visit_results = array_merge($this->default_results[$group_by_index], $visit_results);
+
+
+    $patient_results = Patient::whereDate('start_regimen_date', '<', $to)->get()->toArray();
+    $patient_results = $this->arrayFilterBy($patient_results, $filters);
+    $patient_results = $this->arrayGroupBy($patient_results, $group_by_index);
+    $patient_results = $this->arrayCount($patient_results);
+    $patient_results = array_merge($this->default_results[$group_by_index], $patient_results);
+
+    foreach($patient_results as $patient_key => $patient_value)
+    {
+      $results[$patient_key] = ($patient_value > 0 ? round($visit_results[$patient_key]/$patient_value) : 0);
+    }
 
     return response()->json($results);
   }
